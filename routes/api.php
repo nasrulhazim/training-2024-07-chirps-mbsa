a9fdea5c744883f498ac2b977ead8632f0947989
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-22 15:31:44 +0800

    Added API endpoint get chirp details

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/chirps', [ChirpController::class, 'index']);
+Route::get('/chirps/{chirp}', [ChirpController::class, 'show']);
