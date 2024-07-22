ecb423d7cc9bc9775410a3f96336648f74b4a260
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-23 05:48:24 +0800

    Set chirps api secure

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/chirps', [ChirpController::class, 'index']);
+    Route::get('/chirps/{chirp}', [ChirpController::class, 'show']);
 });
 
-Route::get('/chirps', [ChirpController::class, 'index']);
-Route::get('/chirps/{chirp}', [ChirpController::class, 'show']);
+
