8f1fefa701c60feb8b451210b58799abd1037492
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-07-30 11:16:45 +0800

    Added test:mail command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Mail\WelcomeMail;
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('test:mail', function() {
+    $user = User::first();
+    Mail::to('nasrul@gmail.com')
+        ->send(new WelcomeMail($user));
+    $this->components->info('mail sent');
+});
